3fb196fcfc34ef361bb48a9f761165c38f730a13
morrowchristian <morrowchristian@icloud.com>
2025-10-23 21:04:13 -0500

    Add basic app structure with sidebar

---
+++ MyNotes/MainView.swift
@@ -0,0 +1,33 @@
+//
+//  MainView.swift
+//  MyNotes
+//
+//  Created by Christian Morrow on 10/23/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationSplitView {
+            SidebarView()
+        } detail: {
+            Text("Select a page")
+        }
+    }
+}
+
+struct SidebarView: View {
+    var body: some View {
+        List {
+            Text("Notes")
+            Text("To-Do")
+            Text("Calendar")
+        }
+        .navigationTitle("Workspace")
+    }
+}
+
+#Preview {
+    MainView()
+}
