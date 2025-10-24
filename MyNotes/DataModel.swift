3883b422dda0dfb8aec7bec035ecf9b3b1e71073
morrowchristian <morrowchristian@icloud.com>
2025-10-24 00:32:29 -0500

    Add createdAt to Page and duplicate handling utils

--- MyNotes/DataModel.swift
+++ MyNotes/DataModel.swift
@@ -6,19 +6,21 @@ enum BlockType: String, Codable, CaseIterable {
     case text
     case todo
     case calendar
+    // TODO: Add more types
 }
 
 struct Block: Identifiable, Codable {
     let id: UUID
     var type: BlockType
-    var content: String = ""  // For text/todo
-    var isCompleted: Bool = false  // For todo
-    var events: [Date: String] = [:]  // For calendar: date (midnight) to note
+    var content: String = ""
+    var isCompleted: Bool = false
+    var events: [Date: String] = [:]
 }
 
 struct Page: Identifiable, Codable {
     let id: UUID
     var title: String
+    var createdAt: Date
     var blocks: [Block] = []
 }
 
@@ -38,5 +40,11 @@ class AppData: ObservableObject {
         guard let data = UserDefaults.standard.data(forKey: "pages"),
               let decoded = try? JSONDecoder().decode([Page].self, from: data) else { return }
         pages = decoded
+    }
+    
+    static func formatTimestamp(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d, yyyy h:mm a"
+        return formatter.string(from: date)
     }
 }
